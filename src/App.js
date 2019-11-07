248978a36128f64ac23767bef2dc51460d8ec7cd
nathan-saygers <nathan.saygers@gmail.com>
2019-11-07 15:50:35 -0800

    added back one day functionality

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import "./App.css";
 import axios from "axios";
 import POTD from "./components/PicOfTheDay/POTD"
 import PhotoDetails from "./components/PhotoDetails/PhotoDetails"
+import Header from "./components/header/Header"
 
 function App() {
   const [photoOfTheDay, setPhotoOfTheDay] = useState([])
+  const [photoDay, setPhotoDay] = useState(7);
+
+  const showYesterdaysPhoto = function() {
+    setPhotoDay(photoDay - 1);
+    console.log("hi",photoDay);
+  }
+
+
 
   useEffect(() => {
-    axios.get("https://api.nasa.gov/planetary/apod?api_key=4cXEcaQuRe0H2jVUHyBMwQOrScsgHBsrO33pMdvw")
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=4cXEcaQuRe0H2jVUHyBMwQOrScsgHBsrO33pMdvw&date=2019-11-0${photoDay}`)
     .then(response => {
       console.log(response.data)
       setPhotoOfTheDay(response.data)
@@ -16,11 +25,12 @@ function App() {
     .catch( error => {
       console.log("Photo data not coming through", error);
     })
-  },[]);
+  },[photoDay]);
 
   return (
     <div className="App">
-      <POTD img={photoOfTheDay.url} />
+      <Header />
+      <POTD yesterdaysPhoto={showYesterdaysPhoto} img={photoOfTheDay.url} />
       <PhotoDetails title={photoOfTheDay.title} date={photoOfTheDay.date} explanation={photoOfTheDay.explanation} />
       <p>
         Read through the instructions in the README.md file to build your NASA
--- src/components/PicOfTheDay/POTD.js
+++ src/components/PicOfTheDay/POTD.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
+import { Jumbotron, Button } from 'reactstrap';
 
 export default function POTD(props) {
-  console.log(props.img);
-  
   return (
-  <div>
-    <img className="POTD-img" alt="NASA's photo of the day" src={props.img}/>
-  </div>
+    <div>
+      <Jumbotron>
+        <h1 className="display-3">This! Is! NASA!</h1>
+        <img className="POTD-img" alt="NASA's photo of the day" src={props.img}/>
+        <p className="lead">
+          <Button onClick={() => props.yesterdaysPhoto()} color="primary">Back One Day</Button>
+        </p>
+      </Jumbotron>
+    </div>
   );
-} 
+};
+
+//   return (
+//   <div>
+//     
+//   </div>
+//   );
+// } 
